e32f162f96bb4b6ad7d2f28f30dbdd5c74677c6b
Sam Braam <sam.braam@orbiz.com>
2016-11-28 16:56:47 +0100

    Test deployment commit

--- de.sh
+++ de.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-title="pacifisted"
+title="pacifisted-de"
 
 eval "echo \"$(cat index.html)\"" > index-de.html
--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 . en.sh
-. de-sh
+. de.sh
 
-mv index-en.html website-en/index.html
-mv index-de.html website-de/index.html
+mv index-en.html en/index.html
+mv index-de.html de/index.html
 
 git add -A .
-git commit -m"Deploying website..."
-git subtree push --prefix=website-en website-en gh-pages
-git subtree push --prefix=website-de website-de gh-pages
 
+#git commit -m"Deploying website..."
+#git subtree push --prefix=website-en website-en gh-pages
+#git subtree push --prefix=website-de website-de gh-pages
 
